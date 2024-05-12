fcc7e87fc449379a4ef295493f00bb3795058af2
ald19 <ald19@alu.ua.es>
2024-05-12 15:03:30 +0200

    Added navbar and card loaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.scss'
+import Navbar from './components/navbar/Navbar'
 import Timeline from './pages/Timeline'
 
 const App = () => {
   return (
     <main>
+      <Navbar />
       <Timeline />
     </main>
   )
--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -3,12 +3,13 @@ import './Timeline.scss'
 import useTimeline from '../hooks/useTimeline'
 import { type Post } from '../interfaces/Post'
 import EditCard from '../components/edit-card/EditCard'
+import SkeletonCard from '../components/skeleton-card/SkeletonCard'
 
 const Timeline = () => {
   const { posts, handlerRemovePost, isLoading, selectedPost, handlerEditPost, handlerSetSelectedPost } = useTimeline()
 
   const renderLoader = () => {
-    return <p>Loading...</p>
+    return Array.from({ length: 5 }).map((_, idx) => <SkeletonCard key={idx} />)
   }
 
   const renderPosts = () => {
